b83b0b82d4cf34a5010688778b23cff53ca51e7c
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-04 22:25:46 +1300

    Add RulesEntry component that shows rule id

--- src/components/RulesContainer.tsx
+++ src/components/RulesContainer.tsx
@@ -2,6 +2,7 @@
 
 import { Rule } from '@/api/types';
 import { FC } from 'react';
+import { RulesEntry } from './RulesEntry';
 
 interface RulesContainerProps {
   rules: Rule[];
@@ -11,12 +12,7 @@ export const RulesContainer: FC<RulesContainerProps> = ({ rules }) => {
   return (
     <div className='flex flex-col overflow-scroll w-full gap-2 p-2 border-solid border-2 border-black dark:border-white'>
       {rules.map((rule) => (
-        <div
-          key={rule.id}
-          className='flex flex-col flex-shrink-0 min-h-24 p-2 items-start border-solid border-2 border-black dark:border-white'
-        >
-          {rule.description}
-        </div>
+        <RulesEntry key={rule.id} rule={rule} />
       ))}
     </div>
   );
---
+++ src/components/RulesEntry.tsx
@@ -0,0 +1,15 @@
+import { Rule } from '@/api/types';
+import { FC } from 'react';
+
+interface RulesEntryProps {
+  rule: Rule;
+}
+
+export const RulesEntry: FC<RulesEntryProps> = ({ rule }) => (
+  <div className='flex flex-shrink-0 gap-2 min-h-24 relative items-start border-solid border-2 border-black dark:border-white'>
+    <div className='absolute top-0 flex justify-center bg-slate-300 dark:bg-slate-500 h-full font-bold [writing-mode:vertical-lr]'>
+      {rule.id}
+    </div>
+    <div className='py-2 pl-7'>{rule.description}</div>
+  </div>
+);
