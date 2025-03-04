8dd064f65043451c43d8577c54b83848aa09c0b1
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-04 22:33:31 +1300

    Adjust text sizing for mobile views

--- src/components/RulesEntry.tsx
+++ src/components/RulesEntry.tsx
@@ -6,10 +6,12 @@ interface RulesEntryProps {
 }
 
 export const RulesEntry: FC<RulesEntryProps> = ({ rule }) => (
-  <div className='flex flex-shrink-0 gap-2 min-h-24 relative items-start border-solid border-2 border-black dark:border-white'>
-    <div className='absolute top-0 flex justify-center bg-slate-300 dark:bg-slate-500 h-full font-bold [writing-mode:vertical-lr]'>
+  <div className='flex flex-shrink-0 gap-2 min-h-16 sm:min-h-24 relative items-start border-solid border-2 border-black dark:border-white'>
+    <div className='absolute top-0 flex justify-center text-sm sm:text-base bg-slate-300 dark:bg-slate-500 h-full font-bold [writing-mode:vertical-lr]'>
       {rule.id}
     </div>
-    <div className='py-2 pl-7'>{rule.description}</div>
+    <div className='py-2 pl-6 sm:pl-7 text-sm sm:text-base'>
+      {rule.description}
+    </div>
   </div>
 );
