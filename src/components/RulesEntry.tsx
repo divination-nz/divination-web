bfd447d5142a63da624d69029ee2219b3bb76be8
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-09 14:33:32 +1300

    Adjust height of rules entry

--- src/components/RulesEntry.tsx
+++ src/components/RulesEntry.tsx
@@ -22,14 +22,14 @@ export const RulesEntry: FC<RulesEntryProps> = ({ rule }) => {
   const rulesUrl = constructRulesUrl(rule);
 
   return (
-    <div className='flex flex-col justify-between flex-shrink-0 gap-2 min-h-16 sm:min-h-24 relative items-start border-solid border-2 border-black dark:border-white'>
+    <div className='flex flex-col justify-between flex-shrink-0 gap-2 min-h-16 sm:min-h-20 relative items-start border-solid border-2 border-black dark:border-white'>
       <div className='absolute top-0 flex justify-center text-sm sm:text-base bg-slate-300 dark:bg-slate-500 h-full font-bold [writing-mode:vertical-lr]'>
         {rule.id}
       </div>
       <div className='py-2 pl-6 sm:pl-7 text-sm sm:text-base'>
         {rule.description}
       </div>
-      <div className='pl-6 sm:pl-7 text-sm sm:text-base text-blue-400'>
+      <div className='py-2 pl-6 sm:pl-7 text-sm sm:text-base text-blue-400'>
         <a
           href={rulesUrl}
           target='_blank'
