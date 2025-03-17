63d775f6fdaace7e1e44894701b9bfaae02f5a17
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-17 23:17:46 +1300

    Add page links to Footer for non-mobile

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,14 +1,15 @@
 import { type FC } from 'react';
 import { Icon } from '@iconify/react';
+import Link from 'next/link';
 
 interface FooterProps {
   showCopyleftIcon?: boolean;
 }
 
 export const Footer: FC<FooterProps> = ({ showCopyleftIcon = true }) => (
-  <footer className='flex w-full flex-row flex-wrap items-center justify-between gap-1 p-2 text-sm sm:text-[1rem]'>
+  <footer className='flex w-full flex-row flex-wrap items-center justify-between p-2 text-sm sm:text-[1rem]'>
     <a
-      className='flex items-center gap-2 hover:underline hover:underline-offset-4'
+      className='flex items-center justify-start gap-2 hover:underline hover:underline-offset-4 sm:basis-1/4'
       href='https://github.com/card-spy/divination-web'
       target='_blank'
       rel='noopener noreferrer'
@@ -16,6 +17,34 @@ export const Footer: FC<FooterProps> = ({ showCopyleftIcon = true }) => (
       <Icon icon='mdi:github' width='24' height='24' />
       View Github Repo
     </a>
-    Copyleft {showCopyleftIcon && '🄯'} Matthew Eden {new Date().getFullYear()}
+    <div className='hidden h-full basis-1/2 items-center justify-center gap-2 sm:flex'>
+      <Link className='hover:underline' href='/'>
+        Home
+      </Link>
+      {' ∙ '}
+      <Link className='hover:underline' href='/about'>
+        About
+      </Link>
+      {/* {' ∙ '}
+      <Link className='hover:underline' href='/search'>
+        Search
+      </Link>
+      {' ∙ '}
+      <Link className='hover:underline' href='/glossary'>
+        Glossary
+      </Link> */}
+      {' ∙ '}
+      <Link
+        className='hover:underline'
+        href='https://rules.cardspy.nz'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Rules
+      </Link>
+    </div>
+    <div className='flex h-full items-center justify-end sm:basis-1/4'>
+      Copyleft {showCopyleftIcon && '🄯'} Matthew Eden {new Date().getFullYear()}
+    </div>
   </footer>
 );
