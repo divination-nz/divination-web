df06c36e3a38089a0957358eed3ac6b7e9b082c5
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-04-10 20:26:59 +1200

    Separate server and client components for Glossary
    This isn't necessary right now, but rather prepares for client-specific
    functionality to be added.

--- src/app/glossary/page.tsx
+++ src/app/glossary/page.tsx
@@ -1,38 +1,12 @@
 import { getGlossary } from '@/api/getGlossary';
-import { GlossaryEntry } from '@/components/GlossaryEntry';
-import { Icon } from '@iconify/react';
+import { GlossaryClient } from './GlossaryClient';
 
 export default async function Glossary() {
-  const glossary = await getGlossary();
+  const glossaryTerms = await getGlossary();
 
   return (
-    <main className='flex w-full flex-col items-start justify-start gap-2 overflow-y-scroll lg:px-48'>
-      <h1 className='w-full font-[family-name:var(--font-beleren)] text-6xl text-blue sm:text-center sm:text-7xl'>
-        {'Glossary'}
-      </h1>
-      <div className='relative w-full'>
-        <Icon
-          className='absolute left-2 top-3'
-          icon='material-symbols:search-rounded'
-          width='24'
-          height='24'
-        />
-        <input
-          autoFocus
-          className='h-12 w-full rounded-lg border-2 border-solid border-surface2 bg-mantle px-9 py-0.5 font-[family-name:var(--font-geist-sans)] text-lg text-text [outline:none] hover:border-text focus:border-text'
-          type='text'
-          placeholder='APNAP, keyword ability, subtype...'
-        />
-      </div>
-      <div className='flex w-full flex-wrap gap-2 overflow-x-hidden overflow-y-scroll border-2 border-solid border-text p-2'>
-        {glossary.map((term, index) => (
-          <GlossaryEntry
-            key={index}
-            title={term.term}
-            description={term.description}
-          />
-        ))}
-      </div>
+    <main className='flex h-[calc(100vh-5rem)] w-full lg:px-48'>
+      <GlossaryClient glossaryTerms={glossaryTerms} />
     </main>
   );
 }
