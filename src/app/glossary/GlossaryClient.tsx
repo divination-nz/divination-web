727a12abc7c930e52077d71b3746f30f1c555f23
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-04-10 20:47:49 +1200

    Add filtering of glossary terms

--- src/app/glossary/GlossaryClient.tsx
+++ src/app/glossary/GlossaryClient.tsx
@@ -3,13 +3,21 @@
 import { GlossaryTerm } from '@/api/types';
 import { GlossaryEntry } from '@/components/GlossaryEntry';
 import { Icon } from '@iconify/react';
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 
 interface GlossaryClientProps {
   glossaryTerms: GlossaryTerm[];
 }
 
 export const GlossaryClient: FC<GlossaryClientProps> = ({ glossaryTerms }) => {
+  const [filter, setFilter] = useState<string>('');
+
+  const filteredGlossaryTerms = glossaryTerms.filter((term) => {
+    const termText = term.term.toLowerCase();
+    const descriptionText = term.description.toLowerCase();
+    return termText.includes(filter) || descriptionText.includes(filter);
+  });
+
   return (
     <div className='flex h-full w-full flex-col gap-2'>
       <h1 className='w-full font-[family-name:var(--font-beleren)] text-6xl text-blue sm:text-center sm:text-7xl'>
@@ -27,14 +35,16 @@ export const GlossaryClient: FC<GlossaryClientProps> = ({ glossaryTerms }) => {
           className='h-12 w-full rounded-lg border-2 border-solid border-surface2 bg-mantle px-9 py-0.5 font-[family-name:var(--font-geist-sans)] text-lg text-text [outline:none] hover:border-text focus:border-text'
           type='text'
           placeholder='APNAP, keyword ability, subtype...'
+          onChange={(e) => setFilter(e.target.value)}
         />
       </div>
       <div className='flex w-full flex-wrap gap-2 overflow-x-hidden overflow-y-scroll border-2 border-solid border-text p-2'>
-        {glossaryTerms.map((term, index) => (
+        {filteredGlossaryTerms.map((term, index) => (
           <GlossaryEntry
             key={index}
             title={term.term}
             description={term.description}
+            markedText={filter}
           />
         ))}
       </div>
