47b614dc4988c9565e78829c6aa5cdd696146cbb
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-04-17 20:40:20 +1200

    Mark glossary terms when filter matches

--- src/components/GlossaryEntry.tsx
+++ src/components/GlossaryEntry.tsx
@@ -12,11 +12,19 @@ export const GlossaryEntry: FC<GlossaryEntryProps> = ({
   description,
   markedText,
 }) => {
+  const markedTitle = !!markedText
+    ? title.replace(
+        new RegExp(`\\b(${markedText})\\b`, 'gi'),
+        '<mark>$1</mark>'
+      )
+    : title;
+
   return (
     <div className='w-full rounded-lg border-2 border-text p-2 text-text'>
-      <h2 className='font-[family-name:var(--font-beleren)] text-lg text-mauve sm:text-xl'>
-        {title}
-      </h2>
+      <h2
+        className='font-[family-name:var(--font-beleren)] text-lg text-mauve sm:text-xl'
+        dangerouslySetInnerHTML={{ __html: markedTitle }}
+      />
       <GlossaryDescription description={description} markedText={markedText} />
     </div>
   );
