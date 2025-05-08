b3d82e358d882fb1b7fa435f91e13e5d84c6efe8
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-05-08 20:56:12 +1200

    Clean up next config

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        // this isn't strictly a good practice, but there are too many domains to list out explicitly
-        hostname: '**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
